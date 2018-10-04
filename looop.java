a1438546a53d15f09c97daabda4f73c89fb6aa09
Jiur Zhu <jianing_zhu@aw.org>
2018-10-04 13:35:18 -0700

    looooop example

---
+++ looop.java
@@ -0,0 +1,36 @@
+public class looop{
+
+  public static void main(String[] args) {
+
+//loop for #2
+    int sum = 0;
+    for(int i = x; i < y + 1; i++){
+      sum = sum + i;
+    }
+    System.out.println(sum);
+
+//loop for #3
+    int number = 0;
+    int amount = 0;
+    for(int i = 0; i < stringIndex + 1; i++){
+      i = number;
+      for(int d = 0; d < stringIndex + 1; d++){
+        if(d = i){
+          amount = amount + 1;
+        }
+      }
+    }
+    System.out.println(amount);
+    System.out.println(number);
+
+
+//loop for #5
+    int x = five.nextInt();
+    long yeeee = 1;
+    for(int i = 1; i<=x; i++){
+      yeeee = yeeee * i;
+    }
+    System.out.println(yeeee);
+
+  }
+}
