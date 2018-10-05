6bd330ee64d987ab2170a6e9b2d24132ddd555bd
Jiur Zhu <jianing_zhu@aw.org>
2018-10-04 22:37:13 -0700

    reverse string

---
+++ convert.java
@@ -0,0 +1,33 @@
+//2 methods to convert a stringbuilder intop the reverse
+//first one while loop, second recursive
+import java.util.*;
+
+public class convert{
+
+  public static String reverse(String hey) {
+      if ((null == hey) || (hey.length() <= 1)) {
+          return hey;
+      }
+      return reverse(hey.substring(1)) + hey.charAt(0);
+  }
+
+
+
+  public static void main(String[] args) {
+//just block one of them out when testing, both should work
+//option 1 with loop
+    System.out.println("what do you wanna reverse?");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        StringBuilder ohyeah = new StringBuilder(str);
+        System.out.println("Here ya go:");
+        System.out.println(ohyeah.reverse().toString());
+
+//option 2 with recursion
+        System.out.println("what do you wanna reverse?");
+        Scanner in2 = new Scanner(System.in);
+        String hey = in2.nextLine();
+        String reverseHey = reverse(hey);
+        System.out.println("Here ya go: "+ reverseHey);
+    }
+}
